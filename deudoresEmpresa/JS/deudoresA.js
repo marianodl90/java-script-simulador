4b076a04b9b99f57fe4f8c7010d472cb14a65fd3
Mariano de Luis <marianodeluis1990@gmail.com>
2024-07-01 10:51:42 -0300

    metodo para filtrar listado de deudores para que no se repita

--- deudoresEmpresa/JS/deudoresA.js
+++ deudoresEmpresa/JS/deudoresA.js
@@ -1,21 +1,36 @@
 let listaDAStorage = localStorage.getItem("DeudoresA")
 listaDAStorage = JSON.parse(listaDAStorage)
 
+// metodo filtrar para generar un nuevo array que no repita mas de una vez a deudores categoria A 
+const nuevaLista = []
+function filtrarDeudoresUnicos(deudores){
+    deudores.forEach(deudor=>{
+        const deudorSeleccionado = deudores.find(deudor => deudor.id == deudores.id)
+        nuevaLista.push(deudorSeleccionado)
+        console.log(nuevaLista)
+    })  
+}
+    
+
 
 let deudoresAContainer = document.getElementById("deudoresA-container")
-function filtrarDeudoresA (deudoresAArrays) {
+
+function renderizarDeudoresA (deudoresAArrays) {
     deudoresAArrays.forEach(deudorA => {
         const card = document.createElement("div")
-        card.innerHTML = `<h3>${deudorA.nombre}</h3>
-                         <h3>${deudorA.apellido}</h3>
+        card.innerHTML = `
                          <h3>${deudorA.montoAdeudado}</h3>`
-        deudoresAContainer.appendChild(card)
-        
-        
+        deudoresAContainer.appendChild(card)  
     })
-} 
 
-filtrarDeudoresA(listaDAStorage)
+filtrarDeudores()
+
+}
+
+renderizarDeudoresA(nuevaLista)
+
+
+
 
 
 
