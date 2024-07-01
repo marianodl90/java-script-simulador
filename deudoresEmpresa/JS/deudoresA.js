0c363d14b1abd30f5db0d20826064c52387f63a8
Mariano de Luis <marianodeluis1990@gmail.com>
2024-07-01 14:04:18 -0300

    agregamos que la funcion filtrarDeudoresUnicos devuelva una funcion para hacerla de orden superior

--- deudoresEmpresa/JS/deudoresA.js
+++ deudoresEmpresa/JS/deudoresA.js
@@ -2,29 +2,34 @@ let listaDAStorage = localStorage.getItem("DeudoresA")
 listaDAStorage = JSON.parse(listaDAStorage)
 
 // metodo filtrar para generar un nuevo array que no repita mas de una vez a deudores categoria A 
+
+// funcion "filtrarDeudoresUnicos" retorna una funcion
+
 const nuevaLista = []
-function filtrarDeudoresUnicos(deudores){
-    deudores.forEach(deudor=>{
-        const deudorSeleccionado = deudores.find(deudor => deudor.id == deudores.id)
+
+function filtrarDeudoresUnicos(Arrays){
+    deudores.forEach(deudorA=>{
+        const deudorSeleccionado = deudoresA.find(deudorA => deudorA.id == deudoresA.nombre)
+        
         nuevaLista.push(deudorSeleccionado)
-        console.log(nuevaLista)
+        return console.log(nuevaLista)
     })  
 }
-    
 
+filtrarDeudoresUnicos(listaDAStorage)
+
+// Renderizamos en nueva lista en HTML la lista nueva
 
 let deudoresAContainer = document.getElementById("deudoresA-container")
 
 function renderizarDeudoresA (deudoresAArrays) {
     deudoresAArrays.forEach(deudorA => {
         const card = document.createElement("div")
-        card.innerHTML = `
+        card.innerHTML = `<h3>${deudor.nombre}</h3>
+                         <h3>${deudor.apellido}</h3>
                          <h3>${deudorA.montoAdeudado}</h3>`
         deudoresAContainer.appendChild(card)  
     })
-
-filtrarDeudores()
-
 }
 
 renderizarDeudoresA(nuevaLista)
